86d76626c009d4004b7a186181db574d89578cc9
Aidalestari <aidalestari1410@gmail.com>
2024-01-25 11:32:37 +0700

    [UPDATE] revisi kondisi status 11 approval service

--- src/modules/approval/approval.service.ts
+++ src/modules/approval/approval.service.ts
@@ -347,6 +347,11 @@ export class ApprovalService {
         departmentTo = 'TXC-3';
       } else if (updateRealizationDto.statusToId === 10) {
         personalNumberTo =
+          realization.roleAssignment['SM_TXC']?.personalNumber ?? null;
+        departmentTo =
+          realization.roleAssignment['SM_TXC']?.personalUnit ?? null;
+      } else if (updateRealizationDto.statusToId === 11) {
+        personalNumberTo =
           realization.roleAssignment['vicePresidentTX']?.personalNumber ?? null;
         departmentTo =
           realization.roleAssignment['vicePresidentTX']?.personalUnit ?? null;
@@ -399,10 +404,10 @@ export class ApprovalService {
               item.amountApprove !== null
                 ? item.amountApprove
                 : item.amountCorrection !== null
-                ? item.amountCorrection
-                : item.amountHps !== null
-                ? item.amountHps
-                : null;
+                  ? item.amountCorrection
+                  : item.amountHps !== null
+                    ? item.amountHps
+                    : null;
 
             return await this.prisma.realizationItem.update({
               where: { idRealizationItem: item.idRealizationItem },
