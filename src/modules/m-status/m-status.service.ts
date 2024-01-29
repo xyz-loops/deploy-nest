038f6889d88f1ddbc49f4919d2fefb8bf1981f7f
Aidalestari <aidalestari1410@gmail.com>
2024-01-29 22:02:40 +0700

    [UPDATE] revisi prisma m status

--- src/modules/m-status/m-status.service.ts
+++ src/modules/m-status/m-status.service.ts
@@ -25,6 +25,7 @@ export class MStatusService {
         time: new Date(),
       };
     } catch (error) {
+      console.log(error);
       throw new HttpException(
         {
           data: null,
