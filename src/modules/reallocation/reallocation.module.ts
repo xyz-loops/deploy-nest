9cda8e93c6575e8667c3255dffc6419a8b538924
xyz-loops <marcelinus.fajar@student.com>
2024-03-14 15:28:53 +0700

    [UPDATE] Fix Reallocation

--- src/modules/reallocation/reallocation.module.ts
+++ src/modules/reallocation/reallocation.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReallocationService } from './reallocation.service';
 import { ReallocationController } from './reallocation.controller';
+import { PrismaService } from 'src/core/service/prisma.service';
+import { RoleService } from '../role/role.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [ReallocationController],
-  providers: [ReallocationService],
+  imports: [HttpModule],
+  providers: [ReallocationService, PrismaService, RoleService],
 })
 export class ReallocationModule {}
